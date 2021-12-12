04e6cdd2018e3a9bfdad145c12742805252cf9de
Elif Ay <92391706+aye2864@users.noreply.github.com>
2021-12-12 13:33:48 +0000

    I have added the challenge 1 freezing, cold,hot,warm

--- A3GuessNum.java
+++ A3GuessNum.java
@@ -1,27 +1,80 @@
 /*
-*Name:elif
-*Date: 23/11//21
-*Desription:
-*/
+ *Name: elif
+ *Date: 23/11//21
+ *Description: random number finding game
+ */
 
-import java.util.*;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
 
-public A3GuessNum{
-  
-  
-  //main method
-  public static void main(String[] arg){
-    // Make a scanner and Random Object.
-    Scanner input = new Scanner(System.in);
-    Random numGenerator = new Random();
-    
-    // Generate a random number from 0-5.
-    int answer = numGenerator.nextInt(6);
-    int guess = -1; // default guess num.
-    
-    System.out.println("Guess a number");
-    int guess = input.nextInt();  
-    
-      
-  }// close main
+public class A3GuessNum {
+
+    //main method
+    public static void main(String[] arg) {
+        Random gen = new Random();
+        Scanner input = new Scanner(System.in);
+
+        int playAgain;
+
+        int guess;
+        int random;
+        int range = 100;
+
+        do {
+            System.out.printf("Guess a number between 1 and 100", range);
+
+            random = gen.nextInt(range) + 1;
+
+            do {
+                guess = input.nextInt();
+
+                if (guess == random) {
+                    System.out.println("Correct!");
+                  } else if (guess < 1) {
+                    System.out.println("Too low!");
+                } else if (guess > 100) {
+                    System.out.println("Too high!");
+                } else {
+                    int distance = Math.abs(random - guess);
+                    
+                if (guess < random){
+                  System.out.println("Too low!");
+                }
+                else if(guess > random){
+            System.out.println("Too high!");
+                }
+                    if (distance > 50) {
+                        System.out.println("Freezing");
+                    } else if (distance > 25) {
+                        System.out.println("Cold");
+                    } else if (distance > 10) {
+                        System.out.println("Warm");
+                    } else {
+                        System.out.println("Hot");
+                    }
+                }
+            } while (guess != random);
+
+            do {
+                System.out.println("Do you want to play again? ([Y]es/[N]o)");
+                String answer = input.nextLine().trim().toLowerCase(Locale.ROOT);
+
+                playAgain = 0;
+
+                if (answer.equals("y") || answer.equals("yes")) {
+                    playAgain = 1;
+                }
+
+                if (answer.equals("n") || answer.equals("no")) {
+                    playAgain = 2;
+                }
+            } while (playAgain == 0);
+
+            range = gen.nextInt(50) + 50;
+        } while (playAgain == 1);
+
+        System.out.println("It was fun playing with you");
+        System.out.println("Have a nice day");
+    }
 }
